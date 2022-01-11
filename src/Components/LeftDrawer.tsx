713e118ff01114f008dc6b1f5ea0475ed8a2fc07
Ricardo Rocha <me@rrocha.uk>
2022-01-11 15:50:24 +0000

    * Added a left drawer prototype;

---
+++ src/Components/LeftDrawer.tsx
@@ -0,0 +1,7 @@
+export function LeftDrawer() {
+  return (
+    <div className="hidden bg-slate-300 md:block w-1/4 max-w-[16rem] min-w-[16rem]">
+      ksjhdfsdf
+    </div>
+  );
+}
--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { Nav } from '../Components/Nav';
 import { authenticate } from '../Services/AuthService';
 import { UserStore } from '../Stores/UserStore';
+import { LeftDrawer } from '../Components/LeftDrawer';
+import { Footer } from '../Components/Footer';
 
 export function Layout({ children }: any) {
   const [user, setUser] = useState(UserStore.username);
@@ -11,11 +13,13 @@ export function Layout({ children }: any) {
   });
 
   return (
-    <>
+    <div>
       <Nav user={user} />
-      <div className="p-5 w-full md:w-2/3 lg:w-2/3 xl:w-2/4 mx-auto mb-2 text-center">
-        {children}
+      <div className="flex h-auto">
+        <div className="p-5 w-full md:w-2/3 lg:w-2/4 xl:w-2/4 mx-auto mb-2 text-center">
+          {children}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
