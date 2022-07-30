af1d7223db3abe9c13d695c16c7f64eb360cd7ff
Ricardo Rocha <me@rrocha.uk>
2022-07-30 11:02:40 +0100

    * refactored the UploadAvatar component to handle component logic   only... design and form or any state that it might need; * added two new hooks, useAvatarUpload for handling the avatar selection   and upload, and also useAvatar to handle a global avatar state;

--- src/Components/User/UploadAvatar.tsx
+++ src/Components/User/UploadAvatar.tsx
@@ -1,6 +1,6 @@
 import { Modal } from '../Modal/Modal';
-import { useRef, useState } from 'react';
-import { uploadAvatar } from '../../Services/ListService';
+import { useState } from 'react';
+import { useAvatarUpload } from '../../Hooks/useAvatarUpload';
 
 interface UploadAvatarProps {
   handleClose: () => void;
@@ -8,38 +8,20 @@ interface UploadAvatarProps {
 
 export function UploadAvatar(props: UploadAvatarProps) {
 
-  const [avatar, setAvatar] = useState<any>();
+  const { selectAvatarFile, doUpload, isUploading } = useAvatarUpload();
 
   function closeModal() {
     props.handleClose();
   }
 
-  function submitAvatar(event: any) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    const formData = new FormData();
-    formData.append('avatar', avatar);
-
-    uploadAvatar(formData)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-  function onChange(event: any) {
-    setAvatar(event.target.files[0]);
-  }
-
   return (
     <Modal title="Upload an Avatar" handleModalClose={closeModal}>
-      <form encType="multipart/form-data" onSubmit={submitAvatar}>
-        <input type="file" name="avatar" onChange={onChange}/>
-        <button type="submit">Submit</button>
-      </form>
+      {isUploading ? <div>Uploading file....</div> :
+        <form encType="multipart/form-data" onSubmit={doUpload}>
+          <input type="file" name="avatar" onChange={selectAvatarFile}/>
+          <button type="submit">Submit</button>
+        </form>
+      }
     </Modal>
   );
 }
---
+++ src/Hooks/useAvatar.ts
@@ -0,0 +1,6 @@
+// this hook will be used to show the user avatar...
+// why a hook? so that when we upload a new avatar it is only necessary to update one single source,
+// and then we get a global state update
+export function useAvatar() {
+
+}
